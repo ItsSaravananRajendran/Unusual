05b94a94e611bf278da5835c3ea91693664f630f
thunderbo1t <sarav201997@gmail.com>
2017-08-20 00:40:56 +0530

    Alignment

--- .conky/Script/script.lua
+++ .conky/Script/script.lua
@@ -16,7 +16,7 @@ function conky_main()
         Cpu_graph_center_x = 191
         Cpu_graph_center_y = 189
         Cpu_graph_radius = 47
-        Core_circle_radius = 12
+        Core_circle_radius = 13
         Core_circle_x0=99
         Core_circle_y0=154
         Core_circle_x1=115
@@ -71,17 +71,22 @@ function conky_main()
 end-- end main function
 
 function draw_graph(data,max_value,table_length,blx,bly,red,green,blue,alpha,width,cheight)
-cairo_set_source_rgba (cr,0.5,.75,1,alpha)
-cairo_set_line_width (cr,width)
-    angle=0
+    angle=40
     inc=(360) / table_length
     pro = (30/  max_value)
+    
+
+    draw_start_line(Cpu_graph_radius,65,5,angle)
+    cairo_set_source_rgba (cr,0.5,.75,1,alpha)
+    cairo_set_line_width (cr,width)
+    
     for i=1, table_length do
        bar_height=pro *data[i]
        draw_line_in_circle(Cpu_graph_radius, bar_height, 1, angle)
        angle = angle+inc
        angle = angle % 360
     end
+
 end--of function draw_graph
 
 function draw_line_in_circle(offset, length, width, degree)
@@ -106,4 +111,14 @@ function draw_circle(x,y,r,cpu )
     cairo_arc (cr,x,y,r,End_angel,Start_angle)
     cairo_fill_preserve (cr)
     cairo_stroke (cr)
+end
+
+function draw_start_line(offset,length,width,degree)
+    point = (math.pi / 180) * degree
+    start_x = 0 + (offset * math.sin(point))
+    start_y = 0 - (offset * math.cos(point))
+    end_x = 0 + ((offset + length) * math.sin(point))
+    end_y = 0 - ((offset + length) * math.cos(point))
+    cairo_move_to(cr, start_x +  Cpu_graph_center_x, start_y +  Cpu_graph_center_y)
+    cairo_line_to(cr, end_x +  Cpu_graph_center_x, end_y +  Cpu_graph_center_y)
 end
